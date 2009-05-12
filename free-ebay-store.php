753f418fb9e5f613f4b65190355705e038a68f7e
GeekLad <GeekLad@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-12 00:56:46 +0000

    git-svn-id: https://plugins.svn.wordpress.org/free-ebay-store/tags/1.2@117288 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ free-ebay-store.php
@@ -0,0 +1,137 @@
+<?php
+/*
+Plugin Name: Free eBay Store
+Plugin URI: http://geeklad.com/free-ebay-store
+Description: Display your own ebay store on your blog.  To display a store, use the following code in a page or post:  <code>[ebay campaignid="YOUR-CAMPAIGN-ID" keywords="YOUR-STORE-KEYWORDS" rows="NUMBER-OF-ROWS-YOU-WANT-TO-DISPLAY" columns="NUMBER-OF-COLUMNS-YOU-WANT-TO-DISPLAY" pagination="yes" vertical="yes"]</code>  Use spaces between the keywords.  You can also <a href="widgets.php">display the store as a widget</a>.
+Author: GeekLad
+Version: 1.2
+Author URI: http://geeklad.com/
+License: GPL*/
+
+/*  Copyright 2009 GeekLad (email : geeklad@geeklad.com)    This program is free software; you can redistribute it and/or modify    it under the terms of the GNU General Public License as published by    the Free Software Foundation; either version 2 of the License, or    (at your option) any later version.    This program is distributed in the hope that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    GNU General Public License for more details.    You should have received a copy of the GNU General Public License    along with this program; if not, write to the Free Software    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA*/
+
+add_shortcode('ebay', 'ebay_store_display');
+function ebay_store_display($atts) {
+	extract(shortcode_atts(array(
+			'campaignid' => false,
+			'keywords' => false,
+			'rows' => '5',
+			'columns' => '2',
+			'pagination' => false,
+			'vertical' => false,
+		), $atts));
+		
+		if (!$keywords || !$campaignid)
+			return "";
+		
+		if ($pagination && $pagination != "none") {
+			$pagination = "&pagination=yes";
+			if ($_GET['pagenumber'])
+				$pagination .= "&pagenumber=" . $_GET['pagenumber'];
+			else
+				$pagination .= "&pagenumber=1";
+		}
+		
+		if ($vertical && $vertical != "no") {
+			$vertical = "&display=vertical";
+		}
+		
+		$keywords = urlencode($keywords);
+		$storeurl = "http://geeklad.com/tools/ebay.php?campaignid=$campaignid&rows=$rows&columns=$columns$pagination$vertical&keyword=$keywords";
+		return file_get_contents($storeurl);
+}
+add_action("plugins_loaded", "ebay_store_widget_init");
+function ebay_store_widget_init() {
+	register_widget_control(__('Free eBay Store'), 'ebay_store_widget_control');
+	register_sidebar_widget(__('Free eBay Store'), 'ebay_store_widget');
+}
+
+function ebay_store_widget_control() {
+	$options = $newoptions = get_option('ebay_store_widget');
+	if ( $_POST["ebay_store_widget_submit"] ) {
+		$newoptions['title'] = $_POST["ebay_store_widget_title"];
+		$newoptions['campaignid'] = $_POST["ebay_store_widget_campaignid"];
+		$newoptions['keywords'] = $_POST["ebay_store_widget_keywords"];
+		$newoptions['itemcount'] = $_POST["ebay_store_widget_itemcount"];
+		$newoptions['before'] = str_replace("\"", "'", str_replace("\\", "", $_POST["ebay_store_widget_before"]));
+		$newoptions['after'] = $_POST["ebay_store_widget_after"];
+	}
+	
+	if ( empty($options['keywords']) && empty($options['campaignid']) ) {
+		$title = "Buy on eBay";
+		$keywords = "";
+		$itemcount = 3;
+		$before = "<div style='font-size: 10px; line-height: 12px;'>";
+		$after = "</div>";
+	}
+	else {
+		$title = $newoptions['title'];
+		$campaignid = $newoptions['campaignid'];
+		$keywords = $newoptions['keywords'];
+		$itemcount = $newoptions['itemcount'];
+		$before = $newoptions['before'];
+		$after = $newoptions['after'];
+	}
+	
+	if ( $options != $newoptions ) {
+		$options = $newoptions;
+		update_option('ebay_store_widget', $options);
+	}
+?>
+			<p><label for="ebay-store-widget-title"><?php _e('Title:'); ?> <input class="widefat" name="ebay_store_widget_title" type="text" value="<?php echo $title; ?>" title="This will be the title of the widget on your webpage."/></label></p>
+			<p>
+				<label for="ebay-store-widget-campaignid"><?php _e('Campaign ID<br>'); ?><input type="text" name="ebay_store_widget_campaignid" value="<?php echo $campaignid; ?>" title="Don't forget your eBay campaign ID!" />
+				<br />
+			</p>
+			<p>
+				<label for="ebay-store-widget-keywords"><?php _e('Keywords<br>'); ?><input type="text" name="ebay_store_widget_keywords" value="<?php echo $keywords; ?>" title="You can specify multiple sets of keywords by separating them with a pipe character (|).  For example, to display teddy bears or inflatable pools at random, you would enter they keywords as: teddy bear|inflatable pool"/>
+				<br />
+			</p>
+			<p>
+				<label for="ebay-store-widget-itemcount"><?php _e('# Items to Display<br>'); ?><input type="text" name="ebay_store_widget_itemcount" value="<?php echo $itemcount; ?>" title="The number of items to display in the widget."/>
+				<br />
+			</p>
+			<p>
+				<label for="ebay-store-widget-before"><?php _e('Before Store<br>'); ?><input type="text" name="ebay_store_widget_before" value="<?php echo $before; ?>" title="The html code to use before the widget.  The default choice reduces the font so that it might fit a bit better in a sidebar."/>
+				<br />
+			</p>
+			<p>
+				<label for="ebay-store-widget-after"><?php _e('After Store<br>'); ?><input type="text" name="ebay_store_widget_after" value="<?php echo str_replace("\\", "", $after); ?>" title="Be sure to close the tag you use before the widget." />
+				<br />
+			</p>
+			<input type="hidden" id="ebay_store_widget_submit" name="ebay_store_widget_submit" value="1" />
+<?php
+}
+
+function ebay_store_widget($args) {
+	$options = get_option('ebay_store_widget');
+	if ($options['keywords'] && $options['campaignid']) {
+		if ( '%BEG_OF_TITLE%' != $args['before_title'] ) {
+			if ( $output = wp_cache_get('ebay_store_widget', 'widget') )
+				return print($output);
+			ob_start();
+		}
+
+		extract($args);
+		$keywords = @preg_split("/\|/", $options['keywords']);
+		if (!$keywords)
+			$keywords = $options['keywords'];
+		else
+			$keywords = $keywords[rand(0, count($keywords)-1)];
+		$options['keywords'] = $keywords;
+		$options['vertical'] = "yes";
+		$options['columns'] = 1;
+		$options['rows'] = $options['itemcount'];
+
+		echo $before_widget;
+		echo $before_title . $title . $after_title;
+		echo str_replace("\\", "", $before);
+		echo ebay_store_display($options);
+		echo str_replace("\\", "", $after);
+		echo $after_widget;
+		
+		if ( '%BEG_OF_TITLE%' != $args['before_title'] )
+			wp_cache_add('ebay_store_widget', ob_get_flush(), 'widget');
+	}
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,89 @@
+=== Plugin Name ===
+Contributors: GeekLad
+Donate link: http://geeklad.com
+Tags: ebay, store, earn, money
+Requires at least: 2.0
+Tested up to: 2.7.1
+Stable tag: 1.1
+
+Display an eBay store on your website, and sell products with your own campaign ID. Display them within posts, or as a widget.
+
+== Description ==
+
+The Free eBay Store plugin allows you to automatically display eBay listings on your blog. You just add a simple tag to a post or page, along with your eBay Campaign ID, and the relevant keywords for the products to be displayed. You can also optionally specify the number of rows and columns to specify the layout of your eBay store. Why pay to build an eBay store when you can build one for free?
+
+== Installation ==
+
+= Installation of the Plugin =
+1. Upload `free-ebay-store.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+= Displaying a Store in Pages or Posts =
+If you want to display the store on a page or post, use the following [shortcode](http://faq.wordpress.com/2008/06/18/what-are-the-wordpress-shortcodes/):
+
+	[ebay campaignid="_YourCampaignID_" keywords="_Product Keywords_" rows="_RowCount_" columns="_ColumnCount_" pagination="_yes_" vertical="_yes_"]
+
+Required Parameters:
+
+* campaignid
+* keywords
+
+Be sure to set the _campaignid_ to your own eBay campaign ID.  _keywords_ should contain the keywords for products you want in your store.  Separate the keywords with spaces, and if you want to exclude a particular word, prefix it with a *-* character.  If you want, you can put multiple stores on the same page or post.  Just place multiple shortcodes with the product info you wish to display in each store.
+
+Optional Parameters:
+
+* rows (_Default: 5_)
+* columns (_Default = 2_)
+* pagination (_Default: none_)
+* vertical (_Default: no_)
+
+Set _row_ to the number of product rows you want to display and and _column_ to the number of product columns you want to display.
+
+If you specify the _pagination="yes"_ parameter, it will enable pagination of the store page.  If you have multiple stores on one page, this may cause some odd results if the stores do not have the same row and column count.
+
+If you specify the _vertical="yes"_, it will place the item info below the image rather than to the right of it.
+
+= Displaying a Store as a Widget =
+1. Go to *Appearance* > *Widgets* in WordPress.
+2. Select the section where you want the widget to appear and click *Show*.
+3. Click the *Add* button next to the *Free eBay Store* widget.
+4. Be sure to enter your *Campaign ID* and *Keywords*.  For multiple keywords, separate them with a pipe character (*|*), e.g. _star wars lego|inflatable pool|swingset_.  When multiple keywords are used, one of them will be selected at random each time the widget is displayed.
+5. Select the number of items to display.
+6. Select the HTML code to display before the widget.
+7. Select the HTML code to display after the widget.
+8. Click *Save Changes*.
+
+== Frequently Asked Questions ==
+
+= What's the maximum number of products that can be displayed? = 
+
+25.
+
+= Is it possible to put more than one store on a single page? =
+
+Yes.  Just enter the appropriate shortcode multiple times.
+
+= Why would I want to put more than one store on a single page? =
+
+You may want to display different types of products within the same page.  For instance, if you wanted to display iPods and headphones on the same page, you could use the following shortcode:
+
+	[ebay id="1" campaignid="0123456789" keywords="ipod" columns="2" rows="3"]
+	[ebay id="2" campaignid="0123456789" keywords="headphones" columns="2" rows="3"]
+
+= Why is only one column displayed in the widget? =
+
+I got anxious to release the plugin to the public and got lazy toward the end.  If there is demand to display more than one column, I will add it in.  Most sidebars in WordPress templates will only reasonably fit a single column anyway.
+
+= What's the catch? There are a lot of eBay stores out there that cost money.  Why is yours free? =
+
+* I shamelessly place a link back to the plugin homepage to promote my plugin.
+* 40% of the time, your campaign ID is displayed.  The other 60% of the time, my campaign ID is displayed.
+
+= You only display my campaign ID 40% of the time? What's up with that?  Why won't you display my campaign ID more often? =
+
+As usage of the plugin increases, I will likely increase the display of user campaign IDs.
+
+== Screenshots ==
+
+1. This is a 2-column, 5-row in-line (in a blog post) store, with pagination.
+2. This is a 3-item sidebar widget.
